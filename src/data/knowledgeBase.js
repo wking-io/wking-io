b9d61ab060f0249a693c54ddb2ee4c58952025b4
Will King <contact@wking.io>
2017-08-09 10:02:37 -0500

    Updated knowledgeBase and applied theme.

--- src/data/knowledgeBase.js
+++ src/data/knowledgeBase.js
@@ -4,12 +4,12 @@ const knowledgeBase = [
     book: 'scss',
     chapter: 'How do I make a magical underline that works on line breaks and can be any color I want?',
     content: `
-    .underline--magical {
-      background-image: linear-gradient;
-      background-repeat: none;
-      background-size: 100% 4px // how-thick;
-      background-position: 0 8px // how-far-down;
-    }
+.underline--magical {
+  background-image: linear-gradient;
+  background-repeat: none;
+  background-size: 100% 4px // how-thick;
+  background-position: 0 8px // how-far-down;
+}
     `,
   },
   {
@@ -17,15 +17,10 @@ const knowledgeBase = [
     book: 'javascript',
     chapter: 'How do I get all the siblings of an element?',
     content: `
-    const getSibs = (el) => { 
-      const siblings = []; 
-      for (let sibling = el.parentNode.firstChild ; sibling; sibling = sibling.nextSibling ) { 
-      if ( sibling.nodeType == 1 && sibling != el ) { 
-          siblings.push( sibling ); 
-        } 
-      } 
-      return siblings; 
-    }
+const getSibs = (el) => Array
+  .from(el.parentNode.childNodes);
+  .filter(child => child.nodeType == 1 && child != el);
+}
     `,
   },
   {
@@ -33,33 +28,37 @@ const knowledgeBase = [
     book: 'scss',
     chapter: 'How do I make a magical underline that works on line breaks and can be any color I want?',
     content: `
-    .underline--magical {
-      background-image: linear-gradient;
-      background-repeat: none;
-      background-size: 100% 4px // how-thick;
-      background-position: 0 8px // how-far-down;
-    }
+.underline--magical {
+  background-image: linear-gradient;
+  background-repeat: none;
+  background-size: 100% 4px // how-thick;
+  background-position: 0 8px // how-far-down;
+}
     `,
   },
   {
     _id: '4',
     book: 'javascript',
-    chapter: 'How do I get all the siblings of an element?',
+    chapter: 'How can I transform siblings of an element?',
     content: `
-const transformSibs = (el, callback) => {
-  const siblings = [];
+const transformSibs = (el, callback) => Array
+  .from(el.parentNode.childNodes)
+  .filter(child => child.nodeType == 1 && child != el)
+  .forEach(sibling => {
+    callback(sibling);
+    return sibling;
+  });
+    `,
+  },
+  {
+    _id: '5',
+    book: 'javascript',
+    chapter: 'How can I pass arguments to a callback function?',
+    content: `
+import transformSibs from 'transformSibs';
 
-  for (let sibling = el.parentNode.firstChild ; sibling; sibling = sibling.nextSibling ) {
-    if ( sibling.nodeType == 1 && sibling != el ) {
-      callback(sibling);
-      siblings.push( sibling );
-    }
-  }
-  return siblings;
-}
-  
 // Use a curried function to pass more arguments to callback 
-const removeClass = class => sibling => sibling.classList.remove(class);
+const removeClass = className => el => el.classList.remove(className);
 const $item = document.querySelector('.item');
 transformSibs($item, removeClass('active'));
     `,
