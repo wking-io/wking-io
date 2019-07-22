08ec0093d98e1322a74729ef9e82af242e188b8c
Will King <contact@wking.io>
2019-07-22 15:02:56 -0500

    Final touches

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -10,17 +10,17 @@ import { Newsletter } from "../components/newsletter"
 const Articles = ({ data }) => (
   <Layout>
     <SEO title="Articles" />
-    <div className="max-w-3xl mx-auto pt-40 text-black pb-24">
-      <h2 className="font-display font-bold text-6xl leading-tight mb-12">
+    <div className="max-w-3xl mx-auto px-8 pt-40 text-black pb-24">
+      <h2 className="font-display font-bold text-4xl sm:text-5xl md:text-6xl leading-tight mb-12">
         Articles.
       </h2>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <a
-            className="article-card p-8 block bg-grey-100 mb-6"
+            className="article-card p-6 md:p-8 block bg-grey-100 mb-6"
             href={node.fields.slug}
           >
-            <h3 className="font-display text-black font-bold text-2xl mb-2">
+            <h3 className="font-display text-black font-bold text-xl sm:text-2xl mb-2">
               {node.frontmatter.title}
             </h3>
             <p className="text-grey-600">{node.frontmatter.date}</p>
