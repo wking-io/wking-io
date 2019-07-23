ff47bd7372a1577be8ea35dee6d0475e1d3da648
Will King <contact@wking.io>
2019-07-23 12:24:46 -0500

    Making newsletter require email to submit

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,40 +1,104 @@
 // Dependencies
-import React from "react"
+import React, { useState } from "react"
+import { validate } from "email-validator"
+import { Collapse } from "react-collapse"
 
-export const NewsletterInput = ({ dark = false }) => (
-  <form
-    className="w-full"
-    name="newsletter-signups"
-    method="POST"
-    action="/newsletter-thank-you"
-    netlify-honeypot="bot-field"
-    data-netlify="true"
-  >
-    <div
-      className={`newsletter-input-${
-        dark ? `dark` : `light`
-      } rounded border border-black p-2 flex flex-col md:flex-row justify-between w-full bg-white`}
+const VALID = "valid"
+const EMAIL_ERROR = "error"
+const EMAIL_EMPTY = "empty"
+
+export const NewsletterInput = ({ dark = false }) => {
+  const [valid, setValid] = useState(EMAIL_EMPTY)
+  const validateForm = ({ target }) => {
+    if (target.value && target.value.length >= 0) {
+      if (validate(target.value)) {
+        setValid(VALID)
+      } else {
+        setValid(EMAIL_ERROR)
+      }
+    } else {
+      setValid(EMAIL_EMPTY)
+    }
+  }
+
+  const getInputClass = validState => {
+    if (validState === VALID) {
+      return "cursor-pointer"
+    } else if (validState === EMAIL_ERROR) {
+      return "cursor-not-allowed opacity-50"
+    } else if (validState === EMAIL_EMPTY) {
+      return "cursor-not-allowed opacity-50"
+    }
+    return "cursor-not-allowed opacity-50"
+  }
+
+  const isDisabled = validState => {
+    if (validState === VALID) {
+      return false
+    } else if (validState === EMAIL_ERROR) {
+      return true
+    } else if (validState === EMAIL_EMPTY) {
+      return true
+    }
+    return true
+  }
+
+  const showError = validState => {
+    if (validState === VALID) {
+      return false
+    } else if (validState === EMAIL_ERROR) {
+      return true
+    } else if (validState === EMAIL_EMPTY) {
+      return false
+    }
+    return false
+  }
+
+  return (
+    <form
+      className={`newsletter-${dark ? `dark` : `light`} w-full rounded`}
+      name="newsletter-signups"
+      method="POST"
+      action="/newsletter-thank-you"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
     >
-      <input type="hidden" name="form-name" value="newsletter-signups" />
-      <p class="visually-hidden">
-        <label>
-          Don’t fill this out if you're human: <input name="bot-field" />
-        </label>
-      </p>
-      <input
-        className="pt-2 text-center md:text-left pb-4 md:p-2 flex-1 text-black"
-        type="text"
-        placeholder="Enter email address..."
-        name="Email Address"
-      />
-      <input
-        className="btn btn--primary w-auto px-3 capitalize"
-        type="submit"
-        value="Send me updates"
-      />
-    </div>
-  </form>
-)
+      <div
+        className={`newsletter-input-${
+          dark ? `dark` : `light`
+        } rounded border border-black p-2 flex flex-col md:flex-row justify-between w-full bg-white relative z-20`}
+      >
+        <input type="hidden" name="form-name" value="newsletter-signups" />
+        <p class="visually-hidden">
+          <label>
+            Don’t fill this out if you're human: <input name="bot-field" />
+          </label>
+        </p>
+        <input
+          className="pt-2 text-center md:text-left pb-4 md:p-2 flex-1 text-black"
+          type="text"
+          placeholder="Enter email address..."
+          name="Email Address"
+          onChange={validateForm}
+        />
+        <input
+          className={`btn btn--primary w-auto px-3 capitalize ${getInputClass(
+            valid
+          )}`}
+          type="submit"
+          value="Send me updates"
+          disabled={isDisabled(valid)}
+        />
+      </div>
+      <Collapse
+        className="bg-primary-dark text-white rounded -mt-2 pb-2 relative z-10"
+        isOpened={showError(valid)}
+      >
+        <p className="pt-4 px-4">Your email is invalid. Please try again.</p>
+      </Collapse>
+    </form>
+  )
+}
 
 export const Newsletter = () => (
   <div className="bg-black text-white text-center py-20 px-8 md:px-16">
